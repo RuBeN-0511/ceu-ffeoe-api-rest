ae5cbf19bb1227aaaf4c3c76359398e9961d664e
ruben <ruben>
2026-02-03 19:49:20 +0100

    MONDONGO

---
+++ mondapiBD-api-server/src/main/java/ceu/dam/mondapiBD/MondapiBdApiServerApplication.java
@@ -0,0 +1,13 @@
+package ceu.dam.mondapiBD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MondapiBdApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MondapiBdApiServerApplication.class, args);
+	}
+
+}
---
+++ mondapiBD-api-server/src/test/java/ceu/dam/mondapiBD/MondapiBdApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ceu.dam.mondapiBD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MondapiBdApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

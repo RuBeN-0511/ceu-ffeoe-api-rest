516789f7556fb589ce597d0b9043567e1fd492ed
Flashut <139075536+Flashut@users.noreply.github.com>
2026-02-05 15:56:26 +0100

    Cambio especifico
    cambio nomenclatura

--- mondapiBD-api-server/src/main/java/ceu/dam/mondapiBD/model/TutorDocente.java
+++ mondapiBD-api-server/src/main/java/ceu/dam/mondapiBD/model/TutorDocente.java
@@ -10,7 +10,7 @@ import lombok.Data;
 public class TutorDocente {
 
 	@Id
-	private String idTutor;
+	private String id;
 	private String idUsuario;
 	private String nombreDocente;
 	private Boolean activo;
